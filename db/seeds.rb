47fb44cf4222df1161bc40026379933a7544f6f5
Colman Currie <curriecolman@gmail.com>
2020-01-31 10:31:07 -0800

    create seeding for dog and cat names using the faker:creature

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+1.times do |i|
+  @rescue = Rescue.create!(
+  name: Faker::House.room
+)
+
+50.times do
+  @rescue.animals.new(
+  dogs: Faker::Creature::Dog.name,
+  cats: Faker::Creature::Cat.name,
+  # skater_id: nil
+)
+  @rescue.save
+end
+end
