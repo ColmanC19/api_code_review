8a0377e7c59d8cccdeccb6884bf411c22f01cf4f
Colman Currie <curriecolman@gmail.com>
2020-01-31 10:31:48 -0800

    minor changes to controllers, create a models/rescue.rb

--- app/controllers/rescues_controller.rb
+++ app/controllers/rescues_controller.rb
@@ -1,12 +1,37 @@
 class RescuesController < ApplicationController
 
+
   def index
-    @rescues = {"rescue": "Here are the dogs and cats available: "}
+    @rescues = Rescue.all
     json_response(@rescues)
+  end
+
+  def show
+    @rescue = Rescue.find(params[:id])
+    json_response(@rescue)
+  end
+
+  def create
+    @rescue = Rescue.create(rescue_params)
+    json_response(@rescue)
+  end
+
+  def update
+    @rescue = Rescue.find(params[:id])
+    @rescue.update(rescue_params)
+  end
+
+  def destroy
+    @rescue = Rescue.find(params[:id])
+    @rescue.destroy
   end
 
   private
   def json_response(object, status = :ok)
     render json: object, status: status
+  end
+
+  def rescue_params
+    params.permit(:name)
   end
 end
